fdd38c76e010839012df454c6b30f3f9518db057
virakaiser <viracokaiser@gmail.com>
2025-05-26 15:03:24 +0200

    add command configuration file

---
+++ plugin/src/main/java/net/masternation/wallet/config/Commands.java
@@ -0,0 +1,32 @@
+package net.masternation.wallet.config;
+
+import eu.okaeri.configs.OkaeriConfig;
+import me.masterkaiser.bukkit.command.ArgumentMeta;
+import me.masterkaiser.bukkit.command.CommandMeta;
+import me.masterkaiser.framework.config.YamlConfig;
+
+@YamlConfig(fileName = "commands")
+public class Commands extends OkaeriConfig {
+
+    public String prefix = "master-wpln";
+
+    public CommandMeta wallet = new CommandMeta("wallet")
+            .setUsage("/wallet")
+            .setDescription("Check your wallet.")
+            .setPermission("vira.wallet.wallet")
+            .addArgument("admin", new ArgumentMeta(
+                    "admin",
+                    "/wallet <add/minus/set/get/reload> <!player> <!value>",
+                    "Manage wallet.",
+                    "vira.wallet.wallet.admin",
+                    new String[0]
+            ))
+            .setAliases(new String[] {"wallet"});
+
+    public CommandMeta itemShop = new CommandMeta("itemshop")
+            .setUsage("/itemshop")
+            .setDescription("Check ItemShop offers.")
+            .setPermission("vira.wallet.itemshop")
+            .setAliases(new String[] {"is"});
+
+}
